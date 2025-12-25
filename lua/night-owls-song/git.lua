db3b80db48a5e804e0fbd756b6581625eedc74f2
Andrew Shershunov <mail@evilur.ru>
2025-12-26 02:15:03 +0300

    refactor: Update git.lua

--- lua/night-owls-song/git.lua
+++ lua/night-owls-song/git.lua
@@ -1,3 +1,6 @@
 local set = vim.api.nvim_set_hl
+local c = require('night-owls-song.color')
 
-set(0, '@keyword.gitcommit', { link = 'Function' })
+set(0, '@keyword.gitcommit', { fg = c.blue })
+set(0, '@string.special.path.gitcommit', { fg = c.red })
+set(0, '@markup.heading.gitcommit', { fg = c.green })
