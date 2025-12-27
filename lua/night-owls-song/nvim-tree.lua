4dd1a8903875d01e877f341df1eaf8cb5e043761
Andrew Shershunov <mail@evilur.ru>
2025-12-27 16:55:20 +0300

    feat: Add Normal highlight group to NvimTree

--- lua/night-owls-song/nvim-tree.lua
+++ lua/night-owls-song/nvim-tree.lua
@@ -1,6 +1,8 @@
 local set = vim.api.nvim_set_hl
 local c = require('night-owls-song.color')
 
+set(0, 'NvimTreeNormal', { bg = c.mantle })
+set(0, 'NvimTreeNormalNC', { bg = c.mantle })
 set(0, 'NvimTreeFolderIcon', { fg = c.blue })
 set(0, 'NvimTreeFolderName', { link = 'NvimTreeFolderIcon' })
 set(0, 'NvimTreeOpenedFolderName', { link = 'NvimTreeFolderIcon' })
