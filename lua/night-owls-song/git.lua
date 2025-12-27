bd51a830470ac8dfbb906b2af2a8e28f940297fb
Andrew Shershunov <mail@evilur.ru>
2025-12-27 14:51:11 +0300

    feat: Imptove git highlightning

--- lua/night-owls-song/git.lua
+++ lua/night-owls-song/git.lua
@@ -1,6 +1,11 @@
 local set = vim.api.nvim_set_hl
 local c = require('night-owls-song.color')
 
-set(0, '@keyword.gitcommit', { fg = c.blue })
-set(0, '@string.special.path.gitcommit', { fg = c.red })
-set(0, '@markup.heading.gitcommit', { fg = c.green })
+set(0, 'gitcommitFile', { fg = c.red })
+set(0, 'gitcommitHeader', { fg = c.green })
+set(0, 'gitcommitType', { fg = c.blue })
+
+set(0, '@string.special.path.gitcommit', { link = 'gitcommitFile' })
+set(0, '@markup.heading.gitcommit', { link = 'gitcommitHeader' })
+set(0, '@keyword.gitcommit', { link = 'gitcommitType' })
+set(0, '@markup.link.gitcommit', { fg = c.grey_40, underline = true })
