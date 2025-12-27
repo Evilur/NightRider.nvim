cecb4d0ecb1f2c440ec1a037bb8f0cc301ae2080
Andrew Shershunov <mail@evilur.ru>
2025-12-27 17:38:27 +0300

    refactor: Minor fixes

--- lua/night-owls-song/nvim-tree.lua
+++ lua/night-owls-song/nvim-tree.lua
@@ -2,7 +2,8 @@ local set = vim.api.nvim_set_hl
 local c = require('night-owls-song.color')
 
 set(0, 'NvimTreeNormal', { bg = c.mantle })
-set(0, 'NvimTreeNormalNC', { bg = c.mantle })
+set(0, 'NvimTreeNormalNC', { link = 'NvimTreeNormal' })
+set(0, 'NvimTreeEndOfBuffer', { fg = c.mantle })
 set(0, 'NvimTreeFolderIcon', { fg = c.blue })
 set(0, 'NvimTreeFolderName', { link = 'NvimTreeFolderIcon' })
 set(0, 'NvimTreeOpenedFolderName', { link = 'NvimTreeFolderIcon' })
--- lua/night-owls-song/pmenu.lua
+++ lua/night-owls-song/pmenu.lua
@@ -3,7 +3,7 @@ local c = require('night-owls-song.color')
 
 set(0, 'Pmenu', { bg = c.mantle, fg = c.text })
 set(0, 'PmenuSel', { bg = c.blue })
-set(0, 'PmenuSbar', { })
+set(0, 'PmenuSbar', { link = 'Pmenu' })
 set(0, 'PmenuThumb', { bg = c.crust })
 set(0, 'PmenuKind', { })
 set(0, 'PmenuExtra', { fg = c.grey_40 })
